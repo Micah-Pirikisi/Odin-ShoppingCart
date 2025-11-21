0fef2b451865f4eebf27209c64deddb23d3d06b5
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-11-21 10:42:49 +0200

    Imported all components into App.jsx and created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Shop from "./Shop";
+import Cart from "./Cart";
+import { CartProvider } from "./useCart";
 
-function App() {
-
+export default function App() {
   return (
     <>
-      
+      <CartProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
-  )
+  );
 }
-
-export default App
