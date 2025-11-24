714a2c0b214f2454e01f60911b7b578ea624527a
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-11-24 11:12:28 +0200

    Created api requests in Shop.js

---
+++ src/Shop.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+
+export default function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div className="shop">
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
