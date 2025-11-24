853e10076e348b36c1a14efe4b6433f6f7732e8c
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-11-24 10:53:08 +0200

    Created Navbar with live cart count in Navbar.js

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import { useCart } from "../hooks/useCart";
+
+export default function Navbar() {
+  const { totalItems } = useCart();
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart ({totalItems})</Link>
+    </nav>
+  );
+}
